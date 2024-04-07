bdad030d4dc730f134d3b77a3094fc99477bd7c4
Drago903 <andrew.kosatka@gmail.com>
2024-04-07 15:57:25 +0200

    Reducing code complexity in CustomGui

--- src/main/java/it/mikeslab/widencommons/api/inventory/CustomGui.java
+++ src/main/java/it/mikeslab/widencommons/api/inventory/CustomGui.java
@@ -2,6 +2,7 @@ package it.mikeslab.widencommons.api.inventory;
 
 import it.mikeslab.widencommons.api.inventory.pojo.GuiDetails;
 import it.mikeslab.widencommons.api.inventory.pojo.GuiElement;
+import it.mikeslab.widencommons.api.inventory.pojo.RowPopulationContext;
 import it.mikeslab.widencommons.api.inventory.util.GuiChecker;
 import it.mikeslab.widencommons.api.logger.LoggerUtil;
 import lombok.Data;
@@ -75,7 +76,6 @@ public class CustomGui implements InventoryHolder {
 
 
     public void populateInventory(GuiDetails guiDetails, Inventory inventory) {
-
         Map<Character, GuiElement> elements = guiDetails.getElements();
         String[] layout = guiDetails.getInventoryLayout();
 
@@ -83,55 +83,45 @@ public class CustomGui implements InventoryHolder {
             return;
         }
 
-        // Populating inventory
-
         Map<Character, ItemStack> cashedItems = new HashMap<>();
-
-        // Iterating over the layout, for each row
         int row = 0;
-
         GuiType type = guiDetails.getGuiType();
         int perRowLength = type.getRowLength();
 
-
         for (String s : layout) {
+            RowPopulationContext context = new RowPopulationContext(
+                    elements,
+                    cashedItems,
+                    s,
+                    row,
+                    perRowLength,
+                    inventory
+            );
 
-            // Iterating over the elements, for each key (character)
-            for (Map.Entry<Character, GuiElement> entry : elements.entrySet()) {
+            populateRow(context);
+            row++;
+        }
+    }
 
-                char key = entry.getKey();
-                GuiElement element = entry.getValue();
+    private void populateRow(RowPopulationContext context) {
+        for (Map.Entry<Character, GuiElement> entry : context.getElements().entrySet()) {
+            char key = entry.getKey();
+            GuiElement element = entry.getValue();
 
-                for (int column = 0; column < s.length(); column++) { // Change this line
-                    char c = s.charAt(column); // And this line
+            for (int column = 0; column < context.getRowLayout().length(); column++) {
+                char c = context.getRowLayout().charAt(column);
 
-                    if (c == key) {
-
-                        // Item position in the row
-                        int slot = row * perRowLength + column;
-
-                        ItemStack item;
-
-                        // If the item is already in the cache, we can just add it
-                        if(cashedItems.containsKey(key)) {
-                            item = cashedItems.get(key);
-                        } else {
-
-                            // If not cached
-                            item = element.create();
-                            cashedItems.put(key, item);
-
-                        }
-
-                        inventory.setItem(slot, item);
-
-                    }
+                if (c != key) {
+                    continue;
                 }
 
-                row++;
+                int slot = context.getRow() * context.getPerRowLength() + column;
+                ItemStack item = context.getCashedItems().getOrDefault(key, element.create());
+
+                context.getCashedItems().put(key, item);
+                context.getInventory().setItem(slot, item);
             }
         }
-
     }
 
 
