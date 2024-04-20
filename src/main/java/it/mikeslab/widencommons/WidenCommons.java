08a783c2cf38357c11547d63f5e93951f3e635d1
Drago903 <andrew.kosatka@gmail.com>
2024-04-20 19:04:09 +0200

    Sentry.io logging framework implemented (Optional, configurable)

--- src/main/java/it/mikeslab/widencommons/WidenCommons.java
+++ src/main/java/it/mikeslab/widencommons/WidenCommons.java
@@ -1,6 +1,8 @@
 package it.mikeslab.widencommons;
 
+import io.sentry.Sentry;
 import it.mikeslab.widencommons.api.formatter.FormatUtil;
+import it.mikeslab.widencommons.api.logger.LoggerUtil;
 import lombok.Getter;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -10,8 +12,15 @@ public final class WidenCommons extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
+        saveDefaultConfig();
+
 
         FormatUtil.printStartupInfos(this, "00FF72");
+
+        LoggerUtil.setPluginName(this.getName());
+        if(this.getConfig().getBoolean("sentry.enabled")) {
+            this.initSentry();
+        }
 
     }
 
@@ -19,6 +28,24 @@ public final class WidenCommons extends JavaPlugin implements Listener {
     public void onDisable() {
         // Plugin shutdown logic
 
+    }
+
+    /**
+     * Initialize Sentry
+     */
+    private void initSentry() {
+
+        String version = this.getPluginMeta().getVersion();
+        String pluginName = this.getPluginMeta().getName();
+
+        Sentry.init(options -> {
+            options.setDsn(this.getConfig().getString("sentry.dsn"));
+            options.setRelease(pluginName + "@" + version);
+            options.setEnvironment("development");
+
+            options.setTracesSampleRate(1.0);
+
+        });
     }
 
 }
--- src/main/java/it/mikeslab/widencommons/api/logger/LoggerUtil.java
+++ src/main/java/it/mikeslab/widencommons/api/logger/LoggerUtil.java
@@ -1,7 +1,9 @@
 package it.mikeslab.widencommons.api.logger;
 
+import io.sentry.Sentry;
 import org.bukkit.Bukkit;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.logging.Level;
 
 public class LoggerUtil {
@@ -13,10 +15,31 @@ public class LoggerUtil {
 
     public static void log(Level logLevel, LogSource logSource, Exception exception) {
         Bukkit.getLogger().log(logLevel, "[" + pluginName + "] -> (" + logSource.sourceDisplayName + "): " + exception.getMessage());
+
+        sentryLog(LogType.EXCEPTION, exception, logLevel, logSource);
     }
 
     public static void log(Level logLevel, LogSource logSource, String message) {
         Bukkit.getLogger().log(logLevel, "[" + pluginName + "] -> (" + logSource.sourceDisplayName + "): " + message);
+
+        sentryLog(LogType.MESSAGE, message, logLevel, logSource);
+
+    }
+
+
+    private static void sentryLog(LogType logType, Object logObject, Level logLevel, LogSource logSource) {
+
+        CompletableFuture.runAsync(() -> {
+            Sentry.setTag("level", logLevel.getName());
+            Sentry.setTag("plugin", pluginName);
+            Sentry.setTag("source", logSource.sourceDisplayName);
+            Sentry.setTag("server", Bukkit.getServer().getName());
+
+            switch (logType) {
+                case MESSAGE -> Sentry.captureMessage((String) logObject);
+                case EXCEPTION -> Sentry.captureException((Exception) logObject);
+            }
+        });
     }
 
 
@@ -38,5 +61,11 @@ public class LoggerUtil {
             this.sourceDisplayName = sourceDisplayName;
         }
 
+    }
+
+
+    private enum LogType {
+        EXCEPTION,
+        MESSAGE
     }
 }
