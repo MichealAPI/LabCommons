13a13082c2b76b61d1d5d5e99c713e012da8be70
Michele <michele.cippone01@gmail.com>
2024-06-20 19:21:07 +0200

    LabCommons is now a library

--- src/main/java/it/mikeslab/commons/LabCommons.java
+++ src/main/java/it/mikeslab/commons/LabCommons.java
@@ -2,49 +2,28 @@ package it.mikeslab.commons;
 
 import it.mikeslab.commons.api.chat.ChatMessagingHandler;
 import it.mikeslab.commons.api.chat.ChatMessagingListener;
-import it.mikeslab.commons.api.formatter.FormatUtil;
 import it.mikeslab.commons.api.inventory.CustomGui;
 import lombok.Getter;
-import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import org.apache.logging.log4j.core.config.Configurator;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @Getter
-public final class LabCommons extends JavaPlugin implements Listener {
+public final class LabCommons {
 
     public static final String PLUGIN_NAME = "LabCommons";
 
     private ChatMessagingHandler chatMessagingHandler;
 
-    private BukkitAudiences audiences;
-
-    @Override
-    public void onEnable() {
-
-        // Initialize Adventure
-        this.audiences = BukkitAudiences.create(this);
-
-        saveDefaultConfig();
-
-        FormatUtil.printStartupInfos(this, audiences, "00FF72");
-
-        if(!this.getConfig().getBoolean("mongo-info-logging", false)) {
-            this.disableMongoInfoLogging();
-        }
-
-        this.initialize();
-
+    public void initialize(JavaPlugin instance) {
+        this.chatMessagingHandler = new ChatMessagingListener(instance);
     }
 
-    @Override
-    public void onDisable() {
-
+    public void disable() {
         // close all open customGui inventories
 
         for(Player player : Bukkit.getOnlinePlayers()) {
@@ -57,21 +36,13 @@ public final class LabCommons extends JavaPlugin implements Listener {
             }
 
         }
-
-    }
-
-    private void initialize() {
-
-        // Register Chat Messaging listener
-        this.chatMessagingHandler = new ChatMessagingListener(this);
-
     }
 
 
     /**
      * Set the log level to WARN for the MongoDB driver
      */
-    private void disableMongoInfoLogging() {
+    public static void disableMongoInfoLogging() {
 
         // Set the log level to WARN for the MongoDB driver
         Configurator.setLevel(
