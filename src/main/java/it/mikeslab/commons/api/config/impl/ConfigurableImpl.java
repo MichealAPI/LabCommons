2e185fa766ba8a893277f63a61c56c9700a6e734
Michele <michele.cippone01@gmail.com>
2025-03-07 18:58:32 +0100

    Further ConfigurationImpl semplification

--- src/main/java/it/mikeslab/commons/api/config/impl/ConfigurableImpl.java
+++ src/main/java/it/mikeslab/commons/api/config/impl/ConfigurableImpl.java
@@ -8,14 +8,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 
+@Getter
 public class ConfigurableImpl implements Configurable {
 
     private YamlConfiguration configuration;
 
-    @Getter
     private final File dataFolder;
 
-    @Getter
     private final String configName;
 
     public ConfigurableImpl(File dataFolder, String configName) {
@@ -29,23 +28,6 @@ public class ConfigurableImpl implements Configurable {
     public ConfigurableImpl loadConfiguration() {
         this.configuration = YamlConfiguration.loadConfiguration(this.buildFile());
         return this;
-    }
-
-    public YamlConfiguration getConfiguration() {
-
-        if (this.configuration == null) {
-
-            ConfigurableImpl inst = this.loadConfiguration();
-
-            if (inst.configuration == null) {
-                throw new IllegalStateException("Configuration is null");
-            }
-
-            return inst.configuration;
-
-        }
-
-        return this.configuration;
     }
 
 }
