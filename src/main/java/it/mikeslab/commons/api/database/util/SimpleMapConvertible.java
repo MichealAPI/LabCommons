2d36ca263fdd3b4767f6e7566876264f7ec62c8d
Michele <michele.cippone01@gmail.com>
2025-04-16 19:36:04 +0200

    Refactored version of SimpleMapConvertible

--- src/main/java/it/mikeslab/commons/api/database/util/SimpleMapConvertible.java
+++ src/main/java/it/mikeslab/commons/api/database/util/SimpleMapConvertible.java
@@ -4,144 +4,184 @@ import it.mikeslab.commons.api.database.SerializableMapConvertible;
 import it.mikeslab.commons.api.logger.LogUtils;
 import lombok.*;
 import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
 
-@Data
+@Getter
 @ApiStatus.AvailableSince("2.4.1.5")
 public abstract class SimpleMapConvertible<T, U> implements SerializableMapConvertible<U> {
-
-    private static Set<String> IDENTIFIERS = new HashSet<>();
 
     @Setter
     private T uniqueId;
 
-    @Getter
-    private String uniqueIdentifierName;
+    private final String uniqueIdentifierName;
 
-    @Getter(AccessLevel.PUBLIC)
-    private Map<String, Object> values;
+    private final Set<String> knownIdentifiers; // Instance field for identifiers
 
-    public SimpleMapConvertible() {
+    private final Map<String, Object> data = new HashMap<>();
+
+    protected SimpleMapConvertible(@NotNull Class<? extends SimpleIdentifiers> identifiersClass, @NotNull SimpleIdentifiers uniqueIdentifier) {
+        // Initialize final fields
+        this.uniqueIdentifierName = uniqueIdentifier.getKey();
+        this.knownIdentifiers = Collections.unmodifiableSet(SimpleIdentifiers.identifiers(identifiersClass)); // Initialize instance identifiers safely
+
+        // uniqueId starts as null, must be set later
         this.uniqueId = null;
-        this.values = null;
-
-        this.uniqueIdentifierName = null;
     }
 
-    public SimpleMapConvertible(T uniqueId, String uniqueIdentifierName) {
-        this.uniqueId = uniqueId;
-        this.values = new HashMap<>();
-
-        this.uniqueIdentifierName = uniqueIdentifierName;
-    }
-
-    public U fromMap(Map<String, Object> map) {
+    @Override
+    public U fromMap(@Nullable Map<String, Object> map) {
+        this.data.clear(); // Clear previous values if any
 
         if (map != null) {
+            // Defensive copy
+            Map<String, Object> tempMap = new HashMap<>(map);
 
-            this.setValues(new HashMap<>(map));
+            // Retrieve and set uniqueId from map if structure allows
+            Object idFromMap = tempMap.get(this.uniqueIdentifierName);
+            if (idFromMap != null) {
+                try {
+
+                    this.uniqueId = (T) idFromMap; // Requires T's type matches map value type
+                } catch (ClassCastException e) {
+                    LogUtils.warn(LogUtils.LogSource.DATABASE, "Could not cast uniqueId from map: " + e);
+                }
+            }
+
+            // Remove identifier before storing the rest
+            tempMap.remove(this.uniqueIdentifierName);
+            this.data.putAll(tempMap);
 
             LogUtils.debug(
                     LogUtils.LogSource.DATABASE,
-                    "Retrieved values from Database: " + this.getValues()
+                    "Populated instance from Map. Values: " + this.data
             );
-
-            this.getValues().remove(this.getUniqueIdentifierName());
-
         }
 
-        return (U) this;
+        // Unchecked but standard pattern for builder/fluent style in inheritance
+        @SuppressWarnings("unchecked")
+        U self = (U) this;
+        return self;
     }
 
+    @Override
     public Map<String, Object> toMap() {
         Map<String, Object> map = new HashMap<>();
 
-        map.put(
-                this.getUniqueIdentifierName(),
-                this.getUniqueIdentifierValue()
-        );
-
-        if (values != null && !values.isEmpty()) {
-            LogUtils.debug(
-                    LogUtils.LogSource.DATABASE, // todo add a custom name
-                    String.format("Populating a map based on a %s POJO instance which contains: " + values,
-                            this.getClass().getSimpleName()
-                    )
+        // Handle potential null uniqueId
+        String uniqueIdValue = getUniqueIdentifierValueAsString(); // Use helper
+        if (uniqueIdValue == null) {
+            LogUtils.warn(
+                    LogUtils.LogSource.DATABASE,
+                    String.format("Unique identifier '%s' is null for object %s. Skipping.",
+                            this.uniqueIdentifierName, this.getClass().getSimpleName())
             );
-            map.putAll(values);
+        } else {
+            map.put(this.uniqueIdentifierName, uniqueIdValue); // Store String representation
+        }
+
+
+        if (!this.data.isEmpty()) {
+            LogUtils.debug(
+                    LogUtils.LogSource.DATABASE, // Use specific class name or context if possible
+                    String.format("Populating map for %s instance. Values: %s",
+                            this.getClass().getSimpleName(), this.data) // Consider structured logging
+            );
+            map.putAll(this.data);
         }
 
         return map;
     }
 
-    public String getUniqueIdentifierValue() {
-        return this.uniqueId.toString();
+    /**
+     * Gets the String representation of the unique ID, handling null.
+     * @return String representation or null if uniqueId is null.
+     */
+    @Nullable
+    public String getUniqueIdentifierValueAsString() {
+        return this.uniqueId != null ? this.uniqueId.toString() : null;
     }
 
     @Nullable
     public Set<String> getIdentifiers() {
-        return IDENTIFIERS;
+        return this.knownIdentifiers;
     }
 
     /**
-     * Set the identifiers for this class
-     * @param identifiers the identifiers to set
+     * Retrieves a value from the internal map.
+     * Performs an unchecked cast.
+     *
+     * @param key The key corresponding to the value.
+     * @param <V> The expected type of the value.
+     * @return The value, or null if not found.
+     * @throws ClassCastException if the stored value is not assignable to V.
      */
-    public void setIdentifiers(Set<String> identifiers) {
-        IDENTIFIERS = identifiers;
-    }
-
-    /**
-     * Set the identifiers for this class
-     * @param clazz the class which contains the identifiers in a simple way
-     */
-    public void setIdentifiers(Class<? extends SimpleIdentifiers> clazz) {
-        IDENTIFIERS = SimpleIdentifiers.identifiers(clazz);
-    }
-
+    @Nullable
     protected <V> V getValue(String key) {
 
-        // if it has not been added, skip heavier retrieval
-        if (!IDENTIFIERS.contains(key)) {
+        if (!this.knownIdentifiers.contains(key)) {
+            LogUtils.warn(LogUtils.LogSource.DATABASE, String.format("Key '%s' is not a known identifier for %s", key, this.getClass().getSimpleName()));
+             return null;
+        }
+
+        Object rawValue = this.data.get(key);
+        if (rawValue == null) {
             return null;
         }
 
-        return (V) this.getValues().get(key);
+        try {
+            @SuppressWarnings("unchecked")
+            V castValue = (V) rawValue;
+            return castValue;
+        } catch (ClassCastException e) {
+            LogUtils.warn(
+                    LogUtils.LogSource.DATABASE,
+                    String.format("Type mismatch for key '%s' in %s. Expected %s but found %s.",
+                            key, this.getClass().getSimpleName(), "V", rawValue.getClass().getName()) +
+                    e
+            );
+
+            throw e;
+        }
     }
 
-    protected void setValue(String key, Object value) {
+    /**
+     * Sets a value in the internal map.
+     *
+     * @param key The key.
+     * @param value The value.
+     */
+    protected void setValue(String key, @Nullable Object value) {
 
-        if (this.values == null) {
-            this.values = new HashMap<>();
+        if (!this.knownIdentifiers.contains(key)) {
+             LogUtils.warn(LogUtils.LogSource.DATABASE, String.format("Setting value for non-declared identifier key '%s' in %s", key, this.getClass().getSimpleName()));
         }
 
-        IDENTIFIERS.add(key);
-        this.values.put(key, value);
+        if (value == null) {
+            this.data.remove(key);
+        } else {
+            this.data.put(key, value);
+        }
     }
 
-    /*
-     * Set the value of a specific identifier
-     * @param inst the instance of the identifier
-     * @param value the value to set
-     *
-     * Usage of enum to avoid the need of a string and force
-     * an overall better design in all usages
-     */
-    public void setValue(SimpleIdentifiers inst, Object value) {
+    /* Convenience methods using SimpleIdentifiers */
+
+    public void setValue(@NotNull SimpleIdentifiers inst, @Nullable Object value) {
         this.setValue(inst.getKey(), value);
     }
 
-    /*
-     * Get the value of a specific identifier
-     * @param inst the instance of the identifier
-     *
-     * Usage of enum to avoid the need of a string and force
-     * an overall better design in all usages
-     */
-    public <V> V getValue(SimpleIdentifiers inst) {
+    @Nullable
+    public <V> V getValue(@NotNull SimpleIdentifiers inst) {
         return this.getValue(inst.getKey());
     }
 
+    /**
+     * Provides a read-only view of the internal values map.
+     * @return An unmodifiable map of the current values.
+     */
+    public Map<String, Object> getValuesView() {
+        return Collections.unmodifiableMap(this.data);
+    }
 }
